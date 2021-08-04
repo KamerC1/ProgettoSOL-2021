3b2f7c9796d6bf8c8e0425c4d0608aec91ec650f
KamerC1 <80479199+KamerC1@users.noreply.github.com>
2021-08-04 21:54:12 +0200

    File utili per il progetto

---
+++ sortedList/sortedList.c
@@ -0,0 +1,121 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "sortedList.h"
+
+int insertSortedList(NodoPtr *lPtr, int valore)
+{
+    if(*lPtr == NULL)
+    {
+        NodoPtr nuovoNodo = malloc(sizeof(Nodo));
+        if(nuovoNodo != NULL)
+        {
+            nuovoNodo->dato = valore;
+            nuovoNodo->prossimoPtr = NULL;
+            *lPtr = nuovoNodo;
+
+            return 0;
+        }
+        else
+        {
+            perror("Lista: errore");
+            return -1;
+        }
+    }
+    else if((*lPtr)->dato > valore)
+    {
+        NodoPtr nuovoNodo = malloc(sizeof(Nodo));
+        if(nuovoNodo != NULL)
+        {
+            nuovoNodo->dato = valore;
+            nuovoNodo->prossimoPtr = *lPtr;
+            *lPtr = nuovoNodo;
+
+            return 0;
+        }
+        else
+        {
+            perror("Lista: errore");
+            return -1;
+        }
+    }
+    else
+        insertSortedList(&((*lPtr)->prossimoPtr), valore);
+
+
+}
+
+int deleteSortedList(NodoPtr *lPtr, int val)
+{
+    if (*lPtr != NULL)
+    {
+        if (val == (*lPtr)->dato)
+        {
+            NodoPtr tempPtr = *lPtr;
+            *lPtr = (*lPtr)->prossimoPtr;
+            free(tempPtr);
+
+            return 0;
+        }
+        else
+        {
+            NodoPtr precedentePtr = *lPtr;
+            NodoPtr correntePtr = (*lPtr)->prossimoPtr;
+            while (correntePtr != NULL && correntePtr->dato != val)
+            {
+                precedentePtr = correntePtr;
+                correntePtr = correntePtr->prossimoPtr;
+            }
+            if (correntePtr != NULL)
+            {
+                NodoPtr tempPtr = correntePtr;
+                precedentePtr->prossimoPtr = correntePtr->prossimoPtr;
+                free(tempPtr);
+
+                return 0;
+            }
+        }
+    }
+    puts("Elemento non eliminato dalla lista");
+    return -1;
+}
+
+int findSortedList(NodoPtr lPtr, int valore)
+{
+    if(lPtr == NULL)
+    {
+        return -1;
+    }
+    if(lPtr->dato == valore)
+    {
+        return 0;
+    }
+    if(lPtr->dato > valore)
+    {
+        return -1;
+    }
+    else
+        findSortedList(lPtr->prossimoPtr, valore);
+}
+
+void stampaSortedList(NodoPtr lPtr)
+{
+    if(lPtr != NULL)
+    {
+        printf("%d -> ", lPtr->dato);
+        stampaSortedList(lPtr->prossimoPtr);
+    }
+    else
+        printf("NULL\n");
+}
+
+void freeSortedList(NodoPtr *lPtrF)
+{
+    if(*lPtrF != NULL)
+    {
+        NodoPtr temPtr = *lPtrF;
+        *lPtrF = (*lPtrF)->prossimoPtr;
+        free(temPtr);
+
+        freeSortedList(lPtrF);
+    }
+}
---
+++ sortedList/sortedList.h
@@ -0,0 +1,16 @@
+struct nodo
+{
+    int dato;
+    struct nodo *prossimoPtr;
+};
+typedef struct nodo Nodo;
+typedef Nodo *NodoPtr;
+
+//inserisce un elemento in una lista ordinata (ritorna -1 in caso di errore, 0 altrimenti)
+int insertSortedList(NodoPtr *lPtr, int valore);
+//ritorna -1 se l'elemento non viene trovato, 0 altrimenti)
+int findSortedList(NodoPtr lPtr, int valore);
+//(ritorna -1 in caso di errore, 0 altrimenti)
+int deleteSortedList(NodoPtr *lPtr, int val);
+void stampaSortedList(NodoPtr lPtr);
+void freeSortedList(NodoPtr *lPtrF);
