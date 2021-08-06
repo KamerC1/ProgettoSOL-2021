5bbab428f043372282160d03395a8f81782140c5
KamerC1 <80479199+KamerC1@users.noreply.github.com>
2021-08-06 09:34:43 +0200

    Update sortedList.h

--- sortedList/sortedList.h
+++ sortedList/sortedList.h
@@ -6,8 +6,8 @@ struct nodo
 typedef struct nodo Nodo;
 typedef Nodo *NodoPtr;
 
-//inserisce un elemento in una lista ordinata (ritorna -1 in caso di errore, 0 altrimenti)
-int insertSortedList(NodoPtr *lPtr, int valore);
+//inserisce un elemento in una lista ordinata (in caso di errore, termina il processo)
+void insertSortedList(NodoPtr *lPtr, int valore);
 //ritorna -1 se l'elemento non viene trovato, 0 altrimenti)
 int findSortedList(NodoPtr lPtr, int valore);
 //(ritorna -1 in caso di errore, 0 altrimenti)
