c1fdb7adf545f9eeeefc971bd11ab1a42c88401b
KamerC1 <80479199+KamerC1@users.noreply.github.com>
2021-08-13 20:09:26 +0200

    Aggiunta gestione del file di log.

---
+++ include/log.h
@@ -0,0 +1,23 @@
+#ifndef log_h
+#define log_h
+
+#define LOG_FILE "log.txt"
+
+//stampa sul log l'esito dell'operazione - val == 1 ==> Positovo 
+#define ESOP(text, val)                                                             \
+    if(val == 1)                                                                    \
+    {                                                                               \
+        if(fprintf(storage->logFile, "Esito %s: POSITIVO\n\n", text) == -1)         \
+            exit(errno);                                                            \
+    }                                                                               \
+    else                                                                            \
+    {                                                                               \
+        if(fprintf(storage->logFile, "Esito %s: NEGATIVO\n\n", text) == -1)         \
+            exit(errno);                                                            \
+    }                                                                              \
+
+
+
+void writeLogFd_N_Date(FILE *logFile, int clientFd);
+
+#endif
