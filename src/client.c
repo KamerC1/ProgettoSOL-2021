bd09fe7c2d414a877f102b0d8a107cc706d86adb
KamerC1 <80479199+KamerC1@users.noreply.github.com>
2021-08-15 22:50:30 +0200

    Aggiunto la gestione dei comandi: -w, -l, -u, -c, -p
    Il comando -p è ancora da terminare.

--- src/client.c
+++ src/client.c
@@ -12,13 +12,19 @@
 void runRequest(char optionF, char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF);
 void tokenString(char stringToToken[], NodoQiPtr_string *testaStringPtr, NodoQiPtr_string *codaStringPtr);
 void create_OR_Open_File(char argumentF[]);
-void open_File(char argumentF[]);
 int isNumber(const char* s);
+char *getAbsPath(char *tempString);
+void visitDir(const char stringa[], const char dirnameCache[], int *N);
 
+void gestione_w(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF);
 void gestione_W(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF);
 void gestione_r(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF);
 void gestione_R(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF);
+void gestione_l(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF);
+void gestione_c(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF);
 
+static bool enableStdout;
+#define STAMPA_STDOUT(val) if(enableStdout == true) {val;}
 
 int main(int argc, char *argv[])
 {
@@ -32,7 +38,7 @@ int main(int argc, char *argv[])
     NodoCLP *codaCLPtr = NULL;
 
     char *sockName = NULL;
-    SYSCALL(cmlParsing(&testaCLPtr, &codaCLPtr, argc, argv, &sockName), "cmlParsing")
+    SYSCALL(cmlParsing(&testaCLPtr, &codaCLPtr, argc, argv, &sockName, &enableStdout), "cmlParsing")
 
     //Gestisce l'opzione -f
     if(sockName != NULL)
@@ -63,12 +69,6 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-//bool createFile(const char pathname[])
-//{
-//    if(hasCreatedFile == true)
-//        SYSCALL(openFile(pathname, ))
-//}
-
 void runRequest(char optionF, char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
 {
     switch (optionF)
@@ -80,7 +80,8 @@ void runRequest(char optionF, char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPt
             //Operazione ignorata: già gestita altrove
             break;
         case 'w':
-            PRINT("w");
+//            PRINT("w");
+            gestione_w(argumentF, testaCLPtrF, codaCLPtrF);
             break;
         case 'W':
 //            PRINT("W");
@@ -107,16 +108,18 @@ void runRequest(char optionF, char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPt
             PRINT("t");
             break;
         case 'l':
-            PRINT("l");
+//            PRINT("l");
+            gestione_l(argumentF, testaCLPtrF, codaCLPtrF);
             break;
         case 'u':
             PRINT("u");
             break;
         case 'c':
-            PRINT("c");
+//            PRINT("c");
+            gestione_c(argumentF, testaCLPtrF, codaCLPtrF);
             break;
         case 'p':
-            PRINT("p");
+//            PRINT("p");
             break;
         default:
             puts("ERRORE: runRequest");
@@ -130,7 +133,7 @@ void runRequest(char optionF, char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPt
 void create_OR_Open_File(char argumentF[])
 {
     int isPathPresentVal = isPathPresent(argumentF);
-    SYSCALL(isPathPresentVal, "gestione_W")
+    SYSCALL(isPathPresentVal, "create_OR_Open_File")
 
     if(isPathPresentVal == NOT_PRESENTE)
     {
@@ -142,22 +145,22 @@ void create_OR_Open_File(char argumentF[])
     }
 }
 
-//apre il file identificato da "argumentF" se non è stato già aperto.
-void open_File(char argumentF[])
-{
-    int isPathPresentVal = isPathPresent(argumentF);
-    SYSCALL(isPathPresentVal, "gestione_W")
-
-    if(isPathPresentVal == NOT_APERTO)
-    {
-        SYSCALL(openFile(argumentF, O_OPEN), "Errore")
-    }
-    if(isPathPresentVal == NOT_PRESENTE)
-    {
-        printf("Il file %s non è presente nel fileSystem\n", argumentF);
-        exit(EXIT_FAILURE);
-    }
-}
+////apre il file identificato da "argumentF" se non è stato già aperto.
+//void open_File(char argumentF[])
+//{
+//    int isPathPresentVal = isPathPresent(argumentF);
+//    SYSCALL(isPathPresentVal, "open_File")
+//
+//    if(isPathPresentVal == NOT_APERTO)
+//    {
+//        SYSCALL(openFile(argumentF, O_OPEN), "Errore")
+//    }
+//    if(isPathPresentVal == NOT_PRESENTE)
+//    {
+//        printf("open_File: Il file %s non è presente nel fileSystem\n", argumentF);
+//        exit(EXIT_FAILURE);
+//    }
+//}
 
 void tokenString(char stringToToken[], NodoQiPtr_string *testaStringPtr, NodoQiPtr_string *codaStringPtr)
 {
@@ -170,6 +173,64 @@ void tokenString(char stringToToken[], NodoQiPtr_string *testaStringPtr, NodoQiP
 
         token = strtok_r(NULL, ",", &savePtr);
     }
+}
+
+void gestione_w(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
+{
+    NodoQi_string *testaStringPtr = NULL;
+    NodoQi_string *codaStringPtr = NULL;
+
+    tokenString(argumentF, &testaStringPtr, &codaStringPtr);
+
+    //il prossimo opt è -D (quest'ultimo viene rimosso dalla coda)
+    char *dirnameCache = NULL;
+    if(equalToLptr(*testaCLPtrF, 'D'))
+    {
+        char option;
+        popCoda(testaCLPtrF, codaCLPtrF, &option, &dirnameCache);
+    }
+
+
+    //Faccio la write di ogni elemento di testaStringPtr
+    char *dirnameWrite = popString(&testaStringPtr, &codaStringPtr);
+    int numFileToWrite = -1;
+    if (testaStringPtr != NULL)
+    {
+        char *numToConvert = popString(&testaStringPtr, &codaStringPtr);
+        numFileToWrite = isNumber(numToConvert);
+        free(numToConvert);
+
+        if(numFileToWrite == 0)
+            numFileToWrite = -1; //se "numFileToWrite" è un numero negativo, allora vengono letti tutti i file possibili
+
+        if(testaStringPtr != NULL)
+        {
+            errno = EINVAL;
+            PRINT("Sono stati passati troppi argomenti a -w")
+            freeQueueStringa(&testaStringPtr, &codaStringPtr);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    //==Calcola il path dove lavora il processo==
+    char *processPath = getcwd(NULL, PATH_MAX);
+    size_t pathLength = strlen(processPath); //il +1 è già compreso in MAX_PATH_LENGTH
+    REALLOC(processPath, pathLength + 1) //getcwd alloca "MAX_PATH_LENGTH" bytes
+
+    int countFile = numFileToWrite;
+    visitDir(dirnameWrite, dirnameCache, &countFile);
+    free(dirnameWrite);
+
+    SYSCALL(chdir(processPath), "visitDir: chdir")
+    free(processPath);
+
+    if(dirnameCache != NULL)
+    {
+        free(dirnameCache);
+    }
+
+    STAMPA_STDOUT(printf("File letti: %d\n", numFileToWrite - countFile))
+    STAMPA_STDOUT(puts("Esito: positivo"))
 }
 
 void gestione_W(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
@@ -241,12 +302,10 @@ void gestione_r(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
     char *tempStringPop = popString(&testaStringPtr, &codaStringPtr);
     while (tempStringPop != NULL)
     {
-        //recupero il path assoluto del file. Se il file non esiste, restituisce errore
-        char *realPath = getRealPath(tempStringPop);
-        NULL_SYSCALL(realPath, "gestione_r: getRealPath")
-        free(tempStringPop);
+        //Recupero il path assoluto del file.
+        char *realPath = getAbsPath(tempStringPop);
 
-        open_File(realPath);
+        SYSCALL(openFile(argumentF, O_OPEN), "Errore")
 
         if(dirname != NULL)
         {
@@ -257,8 +316,12 @@ void gestione_r(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
             size_t bytesReadBuffer = 0;
             SYSCALL(readFile(realPath, &readBuffer, &bytesReadBuffer), "Errore")
             printf("Contenuto file: \n%s\n", readBuffer);
-            free(readBuffer);
+
+            if(readBuffer != NULL)
+                free(readBuffer);
         }
+
+
         free(realPath);
         tempStringPop = popString(&testaStringPtr, &codaStringPtr);
     }
@@ -292,6 +355,141 @@ void gestione_R(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
     //se non è specificato -d, allora non si fa nulla.
 }
 
+//Il file deve essere presente nel file system per poter fare la lock
+void gestione_l(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
+{
+    NodoQi_string *testaStringPtr = NULL;
+    NodoQi_string *codaStringPtr = NULL;
+
+    tokenString(argumentF, &testaStringPtr, &codaStringPtr);
+
+    //Faccio la lock di ogni elemento di testaStringPtr
+    char *tempStringPop = popString(&testaStringPtr, &codaStringPtr);
+    while (tempStringPop != NULL)
+    {
+        char *realPath = getAbsPath(tempStringPop);
+
+        SYSCALL(openFile(argumentF, O_OPEN), "Errore")
+
+        STAMPA_STDOUT(puts("\nOperazione: lockFile"))
+        STAMPA_STDOUT(printf("File: %s\n", realPath))
+        if(lockFile(realPath) == -1)
+        {
+            STAMPA_STDOUT(puts("Esito: Negativo"))
+            perror("Errore: gestione_c");
+            freeQueueStringa(&testaStringPtr, &codaStringPtr);
+            free(realPath);
+            exit(EXIT_FAILURE);
+        }
+        STAMPA_STDOUT(puts("Esito: Positivo\n"))
+
+        free(realPath);
+        tempStringPop = popString(&testaStringPtr, &codaStringPtr);
+    }
+
+    assert(testaStringPtr == NULL);
+}
+
+//Il file deve essere presente nel file system per poter fare la unlock
+void gestione_u(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
+{
+    NodoQi_string *testaStringPtr = NULL;
+    NodoQi_string *codaStringPtr = NULL;
+
+    tokenString(argumentF, &testaStringPtr, &codaStringPtr);
+
+    //Faccio la lock di ogni elemento di testaStringPtr
+    char *tempStringPop = popString(&testaStringPtr, &codaStringPtr);
+    while (tempStringPop != NULL)
+    {
+        char *realPath = getAbsPath(tempStringPop);
+
+        SYSCALL(openFile(argumentF, O_OPEN), "Errore")
+
+        STAMPA_STDOUT(puts("\nOperazione: unlockFile"))
+        STAMPA_STDOUT(printf("File: %s\n", realPath))
+        if(unlockFile(realPath) == -1)
+        {
+            STAMPA_STDOUT(puts("Esito: Negativo"))
+            perror("Errore: gestione_c");
+            freeQueueStringa(&testaStringPtr, &codaStringPtr);
+            free(realPath);
+            exit(EXIT_FAILURE);
+        }
+        STAMPA_STDOUT(puts("Esito: Positivo\n"))
+
+        free(realPath);
+        tempStringPop = popString(&testaStringPtr, &codaStringPtr);
+    }
+
+    assert(testaStringPtr == NULL);
+}
+
+void gestione_c(char argumentF[], NodoCLPtr *testaCLPtrF, NodoCLPtr *codaCLPtrF)
+{
+    NodoQi_string *testaStringPtr = NULL;
+    NodoQi_string *codaStringPtr = NULL;
+
+    tokenString(argumentF, &testaStringPtr, &codaStringPtr);
+
+    //Faccio la remove di ogni elemento di testaStringPtr
+    char *tempStringPop = popString(&testaStringPtr, &codaStringPtr);
+    while (tempStringPop != NULL)
+    {
+        char *realPath = getAbsPath(tempStringPop);
+
+        SYSCALL(openFile(argumentF, O_OPEN), "Errore")
+
+        STAMPA_STDOUT(puts("\nOperazione: removeFile"))
+        STAMPA_STDOUT(printf("File: %s\n", realPath))
+        //Non Termina il processo solamente se la remove fallsce perché non non è stato trovato il file
+        if(removeFile(realPath) == -1)
+        {
+            if(errno == ENOENT)
+            {
+                STAMPA_STDOUT(puts("Esito: File non presente nel server"))
+                STAMPA_STDOUT(puts("Esito: Negativo"))
+            }
+            else
+            {
+                STAMPA_STDOUT(puts("Esito: Negativo"))
+                perror("Errore: gestione_c");
+                freeQueueStringa(&testaStringPtr, &codaStringPtr);
+                free(realPath);
+                exit(EXIT_FAILURE);
+            }
+        } else
+        {
+            STAMPA_STDOUT(puts("Esito: Positivo\n"))
+        }
+
+        free(realPath);
+        tempStringPop = popString(&testaStringPtr, &codaStringPtr);
+    }
+
+    assert(testaStringPtr == NULL);
+}
+
+//Recupero il path assoluto del file. Termina se il file non esiste su disco e il path passato non è assoluto.
+// (Funziona solo su sistemi UNIX)
+char *getAbsPath(char *tempString)
+{
+    char *realPath = NULL;
+    if(tempString[0] != '/')
+    {
+        realPath = getRealPath(tempString);
+        NULL_SYSCALL(realPath, "Inserire un path assoluto")
+        free(tempString);
+    }
+    else
+    {
+        realPath = tempString;
+    }
+
+    assert(realPath != NULL);
+    return realPath;
+}
+
 //data una stringa, restituisce l'intero corrispondente
 int isNumber(const char* s)
 {
@@ -312,4 +510,61 @@ int isNumber(const char* s)
 
     errno = EINVAL;
     return -1;
+}
+
+//visita ricorsivamente una directory e, per ogni file, chiama la writeFile
+//Se N è un numero negativo, allora vengono letti tutti i file possibili
+void visitDir(const char stringa[], const char dirnameCache[], int *N)
+{
+    if(*N == 0)
+        return;
+
+    DIR *directory = opendir(stringa);
+    NULL_SYSCALL(directory, "visitDir: opendir")
+
+//    printf("Directory: %s\n", stringa);
+    SYSCALL(chdir(stringa), "visitDir: chdir")
+
+    struct dirent *readDirecotry;
+    errno = 0;
+    while((readDirecotry = readdir(directory)) != NULL)
+    {
+        if(readDirecotry->d_type != DT_DIR)
+        {
+            char *realPath = getRealPath(readDirecotry->d_name);
+            NULL_SYSCALL(realPath, "Errore gestione_W: getRealPath")
+            create_OR_Open_File(realPath);
+
+            //se getSizeFileByte restituisce errore, si ignora il file "realPath"
+            if(getSizeFileByte(realPath) == 0)
+            {
+                *N = *N - 1;
+                SYSCALL(writeFile(realPath, dirnameCache), "Errore")
+            }
+            free(realPath);
+        }
+
+        if(*N == 0)
+        {
+            closedir(directory);
+            return;
+        }
+
+        if(readDirecotry->d_type == DT_DIR && strncmp(readDirecotry->d_name, "..", 255) != 0 && strncmp(readDirecotry->d_name, ".", 255))
+        {
+            visitDir(readDirecotry->d_name, dirnameCache, N);
+        }
+
+        errno = 0;
+    }
+
+    if(errno != 0)
+    {
+        perror("Impossibile leggere il file/dir");
+        exit(EXIT_FAILURE);
+    }
+
+    SYSCALL(chdir(".."), "visitDir: chdir")
+
+    closedir(directory);
 }
